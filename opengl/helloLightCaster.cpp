210a859fcc11baa58fae721464b0b30dd1001cc4
plaining <1078029483@qq.com>
2019-03-15 17:20:52 +0800

    接下来学习光源的不同

--- opengl/helloLightCaster.cpp
+++ opengl/helloLightCaster.cpp
@@ -1,4 +1,3 @@
-/*
 #define STB_IMAGE_IMPLEMENTATION
 #include <glad/glad.h>
 #include <GLFW\glfw3.h>
@@ -283,72 +282,8 @@ unsigned int loadTexture(const char * path)
 	}
 	else
 	{
-		std::cout << "Texture failed to load at path: "<< path << std::endl;
+		std::cout << "Texture failed to load at path: " << path << std::endl;
 	}
 	stbi_image_free(data);
 	return texture1;
 }
-*/
-/*
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoords;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-out vec3 Normal;
-out vec3 FragPos;
-out vec2 TexCoords;
-
-void main()
-{
-gl_Position = projection * view * model * vec4(aPos,1.0f);
-Normal = mat3(transpose(inverse(model))) * aNormal;
-FragPos = vec3(model * vec4(aPos,1.0));
-TexCoords = aTexCoords;
-}
-*/
-/*
-#version 330 core
-struct Material{
-sampler2D diffuse;
-sampler2D specular;
-float shininess;
-};
-uniform Material material;
-struct Light{
-vec3 position;
-vec3 ambient;
-vec3 diffuse;
-vec3 specular;
-};
-uniform Light light;
-
-in vec3 Normal;
-in vec3 FragPos;
-in vec2 TexCoords;
-
-uniform vec3 viewPos;
-
-out vec4 FragColor;
-void main()
-{
-vec3 ambient = light.ambient * texture(material.diffuse,TexCoords).rgb;
-
-vec3 norm = normalize(Normal);
-vec3 lightDir = normalize(light.position - FragPos);
-float diff = max(dot(norm, lightDir), 0.0);
-vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
-
-vec3 viewDir = normalize(viewPos - FragPos);
-vec3 reflectDir = reflect(-lightDir, norm);
-float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
-vec3 specular = light.specular * spec * texture(material.specular,TexCoords).rgb;
-
-vec3 result = ambient + diffuse + specular;
-FragColor = vec4(result,1.0);
-};
-*/
